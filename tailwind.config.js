8d5bbc22b389d90dcf613d1a516865f3805a4e55
sadatpour <sadatpour@gmail.com>
2024-05-06 16:02:16 +0330

    Smart Gym - Weight measurement form

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [`./public/**/*.{html,js}`],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["IRANSans"],
+      },
+      colors: {
+        white: "#ffff",
+        black: "#000",
+        smartgymlight: "#DDE4FE",
+        smartgymprimary: "#232441",
+        smartgymprimarydark: "#19192E",
+        smartgymsecodary: "#52559f",
+        smartgymyellow: "#e7fe58",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
